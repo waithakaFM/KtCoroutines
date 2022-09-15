2fdfbda75ff2a277af7b8bf9bd36a0ebdaac42a9
waithakaFM <franco313mwas@gmail.com>
2022-09-15 10:28:47 +0300

    First commit

---
+++ app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.coroutinesdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    //    keep track of count
+    private var count = 0
+
+    private lateinit var messageTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tvCount)
+        messageTextView = findViewById(R.id.tvMessage)
+        val countButton = findViewById<Button>(R.id.btnCount)
+        val downloadButton = findViewById<Button>(R.id.btnDownload)
+
+        countButton.setOnClickListener {
+            textView.text = count++.toString()
+        }
+        downloadButton.setOnClickListener {
+//            creating coroutine
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+
+    // to simulate a long data task
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+
+//            switch the coroutine from one thread to another
+            withContext(Dispatchers.Main){
+                messageTextView.text = "Downloading user $i"
+            }
+//      slowing down by delaying alter the problem of not reading the messages as fast as they are
+//        produced
+            delay(100)
+        }
+    }
+}
+
